f63d8e02ee1bda2f51982bd77172e13169c82cb5
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-13 13:04:51 -0700

    Updating the copyright and license

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -10,15 +23,22 @@ setup(name='gs.group.member.log',
     description="The log of numbers of members joining and leaving a group",
     long_description=open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "HISTORY.txt")).read(),
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='user, group, member, group member, leave, join, log',
     author='Alice Murphy',
     author_email='alice@onlinegroups.net',
     url='http://groupserver.org',
-    license='ZPL',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.member'],
     include_package_data=True,
@@ -31,10 +51,8 @@ setup(name='gs.group.member.log',
         'Products.GSGroupMember',
         'gs.group.member.join',
         'gs.group.member.leave',
-        # -*- Extra requirements: -*-
     ],
     entry_points="""
     # -*- Entry points: -*-
     """,
 )
-
