15587946cecdc3e59de80de9d2dd4677a8b8ef4a
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-22 08:44:18 -0800

    PEP-8 and switching to Unicode literals

--- gs/group/member/log/interfaces.py
+++ gs/group/member/log/interfaces.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2013, 2016 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -12,10 +12,10 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+from __future__ import absolute_import, unicode_literals, print_function
 from zope.interface import Attribute
 from zope.interface.interface import Interface
 from zope.schema import Date, Dict, Int, List, Text
-from zope.viewlet.interfaces import IViewletManager
 
 
 class IJoinAndLeaveLog(Interface):
@@ -25,20 +25,22 @@ class IJoinAndLeaveLog(Interface):
     groupInfo = Attribute("""A groupInfo instance""")
     membersInfo = Attribute("""A GSGroupMembersInfo instance""")
     queries = Attribute("""A JoinLeaveQuery instance""")
-    events = Dict(title=u'Join and Leave Events',
-      description=u'A dictionary, of which the keys are years.'
-        u'Each corresponding value is another dictionary, of which '
-        u'the keys are months. Each month value is another dictionary, '
-        u'which has two keys: one for joining and one for leaving. Each '
-        u'of these values are a list of joining or leaving events.',
-      required=False)
-    years = List(title=u'Years',
-      description=u'A range of integers, ranging from the current '
-        u'year to the first one for which we have joining or leaving data.',
-      required=False)
-    monthLogs = List(title=u'Monthly Logs',
-      description=u'A list of MonthLog instances',
-      required=False)
+    events = Dict(
+        title='Join and Leave Events',
+        description='A dictionary, of which the keys are years. Each corresponding value is '
+        'another dictionary, of which the keys are months. Each month value is another dictionary, '
+        'which has two keys: one for joining and one for leaving. Each  of these values are a '
+        'list of joining or leaving events.',
+        required=False)
+    years = List(
+        title='Years',
+        description='A range of integers, ranging from the current year to the first one for '
+        'which we have joining or leaving data.',
+        required=False)
+    monthLogs = List(
+        title='Monthly Logs',
+        description='A list of MonthLog instances',
+        required=False)
 
 
 class IMonthLog(Interface):
@@ -46,34 +48,43 @@ class IMonthLog(Interface):
         one particular month
     """
     groupInfo = Attribute("""A groupInfo instance""")
-    year = Int(title=u'Year',
-      description=u'The year',
-      required=True)
-    month = Int(title=u'Month',
-      description=u'The month',
-      required=True)
-    numMembersMonthEnd = Int(title=u'End Number of Members',
-      description=u'The number of members at the end of the month',
-      required=False)
-    events = Dict(title=u'Events',
-      description=u'A dictionary with two keys: one with a list of '
-        'joining events, the other with a list of leaving events',
-      required=False)
-    joinEvents = List(title=u'Join Events',
-      description=u'A list of join events for the month',
-      required=False)
-    numMembersJoined = Int(title=u'Number of Members Joined',
-      description=u'The number of members who joined during the month',
-      required=False)
-    leaveEvents = List(title=u'Leave Events',
-      description=u'A list of leave events for the month',
-      required=False)
-    numMembersLeft = Int(title=u'Number of Members Left',
-      description=u'The number of members who left during the month',
-      required=False)
-    numMembersMonthStart = Int(title=u'Start Number of Members',
-      description=u'The number of members at the start of the month',
-      required=False)
+    year = Int(
+        title='Year',
+        description='The year',
+        required=True)
+    month = Int(
+        title='Month',
+        description='The month',
+        required=True)
+    numMembersMonthEnd = Int(
+        title='End Number of Members',
+        description='The number of members at the end of the month',
+        required=False)
+    events = Dict(
+        title='Events',
+        description='A dictionary with two keys: one with a list of joining events, the other with '
+        'a list of leaving events',
+        required=False)
+    joinEvents = List(
+        title='Join Events',
+        description='A list of join events for the month',
+        required=False)
+    numMembersJoined = Int(
+        title='Number of Members Joined',
+        description='The number of members who joined during the month',
+        required=False)
+    leaveEvents = List(
+        title='Leave Events',
+        description='A list of leave events for the month',
+        required=False)
+    numMembersLeft = Int(
+        title='Number of Members Left',
+        description='The number of members who left during the month',
+        required=False)
+    numMembersMonthStart = Int(
+        title='Start Number of Members',
+        description='The number of members at the start of the month',
+        required=False)
 
 
 class IJoinEvent(Interface):
@@ -81,14 +92,15 @@ class IJoinEvent(Interface):
     """
     userInfo = Attribute("""A userInfo instance""")
     groupInfo = Attribute("""A groupInfo instance""")
-    eventDate = Date(title=u'Event Date',
-      description=u'The joining date',
-      required=True)
-    addingUserInfo = Attribute("Optional: The user who added the member to "
-                                "the group")
-    xhtml = Text(title=u'XHTML',
-      description=u'XHTML description of event',
-      required=True)
+    eventDate = Date(
+        title='Event Date',
+        description='The joining date',
+        required=True)
+    addingUserInfo = Attribute("Optional: The user who added the member to the group")
+    xhtml = Text(
+        title='XHTML',
+        description='XHTML description of event',
+        required=True)
 
 
 class ILeaveEvent(Interface):
@@ -96,24 +108,25 @@ class ILeaveEvent(Interface):
     """
     userInfo = Attribute("""A userInfo instance""")
     groupInfo = Attribute("""A groupInfo instance""")
-    eventDate = Date(title=u'Event Date',
-      description=u'The joining date',
-      required=True)
-    removingUserInfo = Attribute("Optional: The user who removed the member "
-                                    "from the group")
-    xhtml = Text(title=u'XHTML',
-      description=u'XHTML description of event',
-      required=True)
+    eventDate = Date(
+        title='Event Date',
+        description='The joining date',
+        required=True)
+    removingUserInfo = Attribute("Optional: The user who removed the member from the group")
+    xhtml = Text(
+        title='XHTML',
+        description='XHTML description of event',
+        required=True)
 
 
 class ILogContentProvider(Interface):
     """The content provider for the group membership log """
     log = Attribute("""A JoinAndLeaveLog instance """)
-    pageTemplateFileName = Text(title=u"Page Template File Name",
-      description=u'The name of the ZPT file that is used to render the log.',
-      required=False,
-      default=u"browser/templates/standardView.pt")
+    pageTemplateFileName = Text(
+        title="Page Template File Name",
+        description='The name of the ZPT file that is used to render the log.',
+        required=False,
+        default="browser/templates/standardView.pt")
 
-
-class IJoinAndLeaveLog(IViewletManager):
-    pass
+# class IJoinAndLeaveLog(IViewletManager):
+#    pass
